d66c55e33b30751758420bbbb6bb34b5ef0096a5
Manoj Sahu <manoj_sahu@outlook.com>
2023-06-24 11:39:15 +0530

    spring boot docker init

---
+++ src/main/java/com/manoj/springboot/docker/DockerImageForDummySpringBootAppApplication.java
@@ -0,0 +1,13 @@
+package com.manoj.springboot.docker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockerImageForDummySpringBootAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DockerImageForDummySpringBootAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/manoj/springboot/docker/DockerImageForDummySpringBootAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.manoj.springboot.docker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DockerImageForDummySpringBootAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
